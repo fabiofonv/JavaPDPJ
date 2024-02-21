21d53f3dd545a7f3bfc997f63f2e9431a660e792
fabiofonv <fabiofonv@hotmail.com>
2024-02-21 20:38:58 -0300

    a

--- src/App.java
+++ src/App.java
@@ -1,5 +1,23 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numeroDeCasos = 10;
+        int casoAtual = 1;
+
+        while (casoAtual <= numeroDeCasos) {
+            //código para imprimir o caso atual
+            System.out.println("Caso" + casoAtual + ":");
+
+            //Veredito: 1 - inocente, 2 - culpado
+            int veredito = (casoAtual % 2 == 0)? 2 : 1;
+            if(veredito == 1) {
+                //código para imprimir o veredito
+                System.out.println("Veredito: Inocente");
+            } else {
+                //código para imprimir o veredito
+                System.out.println("Veredito: Culpado");
+            }
+        }
+        System.out.println();
+        casoAtual++;
     }
 }
