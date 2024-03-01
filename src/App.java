c4b116bafc87a0707789910c33301718bb31bad1
fabiofonv <fabiofonv@hotmail.com>
2024-03-01 20:09:23 -0300

    -

--- src/App.java
+++ src/App.java
@@ -1,28 +1,67 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        int numeroDeCasos = 10;
-        int casoAtual = 1;
-        int inocentes = 0, culpados = 0;
+        Scanner scanner = new Scanner(System.in);
 
-        while (casoAtual <= numeroDeCasos) {
-            //código para imprimir o caso atual
-            System.out.println("Caso" + casoAtual + ":");
+        // Entrada e saída básica de dados, para realizar o registro de um caso judicial
+        System.out.println("Registrar novo caso? (S/N) ");
+        char reply = scanner.nextLine().toUpperCase().charAt(0);
+        boolean cont = 'S' == reply? true: false;
+        
+        Caso caso = new Caso(1);
+        
+        while(cont) {
+            //scanner.nextLine();
+            System.out.println("Digite a matrícula do juiz: ");
+            caso.setMatJuiz(scanner.nextInt());
 
-            //Veredito: 1 - inocente, 2 - culpado
-            int veredito = (int) (Math.random() * 2) + 1;//(casoAtual % 2 == 0)? 2 : 1;
-            if(veredito == 1) {
-                //código para imprimir o veredito
-                System.out.println("Veredito: Inocente");
-                inocentes++;
-            } else {
-                //código para imprimir o veredito
-                System.out.println("Veredito: Culpado");
-                culpados++;
-            }
-            System.out.println();
-            casoAtual++;
-        }      
+            System.out.println("Digite a OAB do advogado de acusação: ");
+            caso.setOabAdvAcu(scanner.nextLine());
 
-        System.out.println("Inocentes: " + inocentes + "\nCulpados: " + culpados);
+            System.out.println("Digite a OAB do advogado de defesa: ");
+            caso.setOabAdvDef(scanner.nextLine());
+
+            System.out.println("Digite o cpf do reclamado: ");
+            caso.setCpfReclamado(scanner.nextInt());
+
+            System.out.println("Digite o cpf do reclamante: ");
+            caso.setCpfReclamante(scanner.nextInt());
+            
+            caso.imprimir();
+
+            System.out.println("Registrar novo caso? (S/N) ");
+            reply = scanner.nextLine().toUpperCase().charAt(0);
+            cont = 'S' == reply? true: false;
+
+        }
+        
+        scanner.close();
     }
 }
+
+// Atividades - Trilha 1
+// int numeroDeCasos = 10;
+//         int casoAtual = 1;
+//         int inocentes = 0, culpados = 0;
+
+//         while (casoAtual <= numeroDeCasos) {
+//             //código para imprimir o caso atual
+//             System.out.println("Caso" + casoAtual + ":");
+
+//             //Veredito: 1 - inocente, 2 - culpado
+//             int veredito = (int) (Math.random() * 2) + 1;//(casoAtual % 2 == 0)? 2 : 1;
+//             if(veredito == 1) {
+//                 //código para imprimir o veredito
+//                 System.out.println("Veredito: Inocente");
+//                 inocentes++;
+//             } else {
+//                 //código para imprimir o veredito
+//                 System.out.println("Veredito: Culpado");
+//                 culpados++;
+//             }
+//             System.out.println();
+//             casoAtual++;
+//         }      
+
+//         System.out.println("Inocentes: " + inocentes + "\nCulpados: " + culpados);
--- src/Caso.java
+++ src/Caso.java
@@ -1,3 +1,70 @@
 public class Caso {
+    private int numero;
+    private int matJuiz;
+    private String oabAdvAcu;
+    private String oabAdvDef;
+    private int cpfReclamado;
+    private int cpfReclamante;
+
+    public Caso(int numero) {
+        this.numero = numero;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getMatJuiz() {
+        return matJuiz;
+    }
+
+    public void setMatJuiz(int matJuiz) {
+        this.matJuiz = matJuiz;
+    }
+
+    public String getOabAdvAcu() {
+        return oabAdvAcu;
+    }
+
+    public void setOabAdvAcu(String oabAdvAcu) {
+        this.oabAdvAcu = oabAdvAcu;
+    }
+
+    public String getOabAdvDef() {
+        return oabAdvDef;
+    }
+
+    public void setOabAdvDef(String oabAdvDef) {
+        this.oabAdvDef = oabAdvDef;
+    }
+
+    public int getCpfReclamado() {
+        return cpfReclamado;
+    }
+
+    public void setCpfReclamado(int cpfReclamado) {
+        this.cpfReclamado = cpfReclamado;
+    }
+
+    public int getCpfReclamante() {
+        return cpfReclamante;
+    }
+
+    public void setCpfReclamante(int cpfReclamante) {
+        this.cpfReclamante = cpfReclamante;
+    }
+
+    public void imprimir() {
+        System.out.println("Nº                  : ");
+        System.out.println("Matrícula do Juiz   : ");
+        System.out.println("Advogado de Defesa  : ");
+        System.out.println("CPF do(a) Reclamado : ");
+        System.out.println("Advogado de Acusação: ");
+        System.out.println("CPF do(a) Reclamante: ");
+    }
     
 }
